2f805383de4920844870e1c04b9ae4bcc38a9be9
Andrey V <Andreyxvv@hotmail.com>
2023-02-19 22:23:42 +0500

    first commit

---
+++ HomeWorkCPP2.8.1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int function(std::string& str, int& forbidden_length) {
+    if (forbidden_length == str.length()) {
+        return 0;
+    }
+    else {
+        return str.length();
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string str;
+    int forbidden_length = 0, bad_length = 0;
+    std::cout << "Введите запретную длину: ";
+    std::cin >> forbidden_length;
+    do {
+        std::cout << "Введите слово: ";
+        std::cin >> str;
+        bad_length = function(str, forbidden_length);
+        if (bad_length != 0) {
+            std::cout << "Длина слова " << '"' << str << '"' << " равна " << bad_length << std::endl;
+        }
+    } while (bad_length != 0);
+    std::cout << "Вы ввели слово запретной длины! До свидания" << std::endl;
+}
