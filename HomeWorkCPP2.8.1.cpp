7ad892f7b8f043908f5d996eb24f7f7b04708315
Andrey V <Andreyxvv@hotmail.com>
2023-02-22 00:29:11 +0500

    error correction

--- HomeWorkCPP2.8.1.cpp
+++ HomeWorkCPP2.8.1.cpp
@@ -1,6 +1,13 @@
 ﻿#include <iostream>
+#include <exception>
 #include <windows.h>
 
+class FunctionError :public std::exception {
+public:
+    const char* what() const override {
+        return "Вы ввели слово запретной длины! До свидания";
+    }
+};
 int function(std::string& str, int& forbidden_length) {
     if (forbidden_length == str.length()) {
         return 0;
@@ -18,13 +25,20 @@ int main()
     int forbidden_length = 0, bad_length = 0;
     std::cout << "Введите запретную длину: ";
     std::cin >> forbidden_length;
-    do {
-        std::cout << "Введите слово: ";
-        std::cin >> str;
-        bad_length = function(str, forbidden_length);
-        if (bad_length != 0) {
-            std::cout << "Длина слова " << '"' << str << '"' << " равна " << bad_length << std::endl;
-        }
-    } while (bad_length != 0);
-    std::cout << "Вы ввели слово запретной длины! До свидания" << std::endl;
+    try {
+        do {
+            std::cout << "Введите слово: ";
+            std::cin >> str;
+            bad_length = function(str, forbidden_length);
+            if (bad_length == 0) {
+                throw FunctionError();
+            }
+            if (bad_length != 0) {
+                std::cout << "Длина слова " << '"' << str << '"' << " равна " << bad_length << std::endl;
+            }
+        } while (true);
+    }
+    catch (const FunctionError& ex) {
+        std::cout << ex.what() << std::endl;
+    }
 }
